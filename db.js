88bc6f4ca23551eccef5251093727c13a8a1d4ce
Md. Sajidur Rahman <62369016+sajidul-jami@users.noreply.github.com>
2025-07-16 17:02:06 +0600

    Update db.js

--- db.js
+++ db.js
@@ -1,12 +1,12 @@
-// db.js
 const mysql = require('mysql2/promise');
-//require('dotenv').config();
 
 const pool = mysql.createPool({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
-  password: process.env.DB_PASS,
+  password: process.env.DB_PASSWORD, // ✅ Fix this line
   database: process.env.DB_NAME,
+  port: process.env.DB_PORT,         // ✅ Optional, but safer
 });
 
 module.exports = pool;
+
